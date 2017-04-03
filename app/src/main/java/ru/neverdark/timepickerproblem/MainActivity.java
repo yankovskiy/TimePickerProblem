37429da0223f3fa6a380c53579da18042be3e38d
Artem Yankovskiy <artemyankovskiy@gmail.com>
2017-04-03 10:23:01 +1000

    workaround from azizbekian
    http://stackoverflow.com/questions/43107964/timepicker-bug-any-workaround/43161326#43161326

--- app/src/main/java/ru/neverdark/timepickerproblem/MainActivity.java
+++ app/src/main/java/ru/neverdark/timepickerproblem/MainActivity.java
@@ -1,18 +1,18 @@
 package ru.neverdark.timepickerproblem;
 
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
 
+import java.lang.reflect.Field;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mButton;
     private TimePicker mTimePicker;
 
     private static final String TAG = "MainActivity";
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton = (Button) findViewById(R.id.reset);
+        Button mButton = (Button) findViewById(R.id.reset);
         mTimePicker = (TimePicker) findViewById(R.id.timePicker);
 
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
                 int hourFromPicker;
                 int minuteFromPicker;
+
+                fixValues();
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     mTimePicker.setHour(currentHour);
@@ -51,5 +53,37 @@ public class MainActivity extends AppCompatActivity {
                 Log.v(TAG, "onClick minute: " + currentMinute + "=>" + minuteFromPicker);
             }
         });
+    }
+
+    private void fixValues() {
+        // bug is not reproducible in APIs 24 and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) return;
+        try {
+            int hour, minute;
+            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+                hour = mTimePicker.getHour();
+                minute = mTimePicker.getMinute();
+            } else {
+                hour = mTimePicker.getCurrentHour();
+                minute = mTimePicker.getCurrentMinute();
+            }
+
+            Field mDelegateField = mTimePicker.getClass().getDeclaredField("mDelegate");
+            mDelegateField.setAccessible(true);
+            Class<?> clazz;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                clazz = Class.forName("android.widget.TimePickerClockDelegate");
+            } else {
+                clazz = Class.forName("android.widget.TimePickerSpinnerDelegate");
+            }
+            Field mInitialHourOfDayField = clazz.getDeclaredField("mInitialHourOfDay");
+            Field mInitialMinuteField = clazz.getDeclaredField("mInitialMinute");
+            mInitialHourOfDayField.setAccessible(true);
+            mInitialMinuteField.setAccessible(true);
+            mInitialHourOfDayField.setInt(mDelegateField.get(mTimePicker), hour);
+            mInitialMinuteField.setInt(mDelegateField.get(mTimePicker), minute);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
